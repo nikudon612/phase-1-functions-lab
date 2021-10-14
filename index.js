0bbbf136ced4efa2e4494f30fd620c6de64adfdb
Nick Bechtel <bechtel.nick@gmail.com>
2021-10-14 08:51:19 -0400

    commit tree a06cca3dc1903c66a9348d6d4ac28b4ee3b3202f

--- index.js
+++ index.js
@@ -31,31 +31,27 @@ function distanceTravelledInFeet(spot1, spot2) {
 //   } else if (n > 400 && n <= 2000) {
 //     // return (n - 400) * 0.02;
 //     return 2.56;
-//   } else if (n > 2000) {
-//     return 2.56;
+//   } else if (n > 2000 && n <= 2499) {
+//     return (n - 400) * 0.2;
 //   } else if (n > 2500) {
 //     return "cannot travel that far";
 //   }
 // }
 
-
-function calculatesFarePrice(start, destination){
-    let calculatesFarePrice;
-    let calculatesFeet= distanceTravelledInFeet(start, destination);
-   if (calculatesFeet < 400 ){
-        return calculatesFarePrice = 0;
-   } else if (calculatesFeet > 400 && calculatesFeet < 2000){
-       calculatesFarePrice = calculatesFeet - 400;
-       return (calculatesFarePrice * 0.02);
-   }
-   else if (calculatesFeet > 2000 && calculatesFeet < 2499){
-       calculatesFarePrice = 25;
-       return calculatesFarePrice;
-   }
-   else if (calculatesFeet > 2500){
-       return 'cannot travel that far';
-   }
-
+function calculatesFarePrice(start, destination) {
+  let calculatesFarePrice;
+  let calculatesFeet = distanceTravelledInFeet(start, destination);
+  if (calculatesFeet < 400) {
+    return (calculatesFarePrice = 0);
+  } else if (calculatesFeet > 400 && calculatesFeet < 2000) {
+    calculatesFarePrice = calculatesFeet - 400;
+    return calculatesFarePrice * 0.02;
+  } else if (calculatesFeet > 2000 && calculatesFeet < 2499) {
+    calculatesFarePrice = 25;
+    return calculatesFarePrice;
+  } else if (calculatesFeet > 2500) {
+    return "cannot travel that far";
+  }
 }
 
 calculatesFarePrice(34, 32);
